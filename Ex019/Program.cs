ab615e39dc07807826a52c661af3f2ebc82f4680
AnimewkaSes <brol0526@gmail.com>
2022-12-06 02:40:24 +0600

    Задание 19

---
+++ Ex019/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+internal class Program
+
+{
+    private static void Main(string[] args)
+    {
+        char rest;
+    do{
+        Console.Clear();
+        Console.WriteLine("Программа для определения пятизначного числа на палиндром");
+        Console.WriteLine("Введите число");
+        string num = Console.ReadLine();
+        string revs = "";
+        char [] numRevs =  num.ToCharArray();
+        Array.Reverse(numRevs);
+        revs = new string(numRevs);
+
+        bool result = num.Equals(revs);
+
+        if (result)
+        {
+            Console.WriteLine("Число является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine("Число не является полиндромом");
+        }
+        Console.Write("Для продолжения введите y, для завершения любая клавиша: ");
+rest = Convert.ToChar(Console.ReadLine());
+}
+while (rest =='y');
+}
+}
